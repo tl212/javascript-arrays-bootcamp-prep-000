e29e5c39fd380ac3e6a10d70e4c9af092cd805b7
T L <tom.kennedy.lee@gmail.com>
2018-06-19 18:27:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,7 @@ function destructivelyRemoveElementFromEndOfArray (chocolateBars, element) {
 }
 
 function removeElementFromEndOfArray (array) {
-  return array.slice(0, array.length-1) 
+  return array.slice(0, array.length - 1) 
 }
 
 
